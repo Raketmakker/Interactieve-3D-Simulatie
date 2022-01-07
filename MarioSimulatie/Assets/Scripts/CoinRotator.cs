56f0c16d0c319803e27d4a41335691e76c390528
Jasper <j.bonefaas@student.avans.nl>
2022-01-07 13:04:27 +0100

    Added project, terrain, models, rotating coin

---
+++ MarioSimulatie/Assets/Scripts/CoinRotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotator : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.fixedDeltaTime);
+    }
+}
